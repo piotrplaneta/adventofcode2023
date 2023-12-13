6ca6d90e910ac2523ef21a576f2e324d4ca8d7a3
piotrplaneta <piotr.planeta@gmail.com>
2023-12-13 16:31:07 +0100

    Fix wrong format

--- lib/day12.swift
+++ lib/day12.swift
@@ -5,15 +5,10 @@ struct RowAndConditions: Hashable {
   let conditions: [Int]
 }
 
-var calculatedWays: [RowAndConditions: Int] = [:]
+var calculatedWays: [RowAndConditions : Int] = [:]
 
 let input = try String(contentsOf: URL(fileURLWithPath: "../input/day12"))
-  .split(separator: "\n").map {
-    (
-      $0.split(separator: " ")[0],
-      $0.split(separator: " ")[1].split(separator: ",").map { Int($0)! }
-    )
-  }
+  .split(separator: "\n").map { ($0.split(separator: " ")[0], $0.split(separator: " ")[1].split(separator: ",").map { Int($0)! }) }
 
 func waysOfPuttingSprings(_ rowAndConditions: (Substring.SubSequence, [Int])) -> Int {
   let row = rowAndConditions.0
@@ -23,36 +18,33 @@ func waysOfPuttingSprings(_ rowAndConditions: (Substring.SubSequence, [Int])) ->
     return calculatedWays[RowAndConditions(row: row, conditions: conditions)]!
   } else {
     let calculated = switch (row, conditions) {
-    case (_, []) where row.allSatisfy { $0 != "#" }: 1
-    case (_, []): 0
-    case (_, _) where row.first == ".": waysOfPuttingSprings((row.dropFirst(), conditions))
-    case (_, _) where row.first == "#":
-      {
+      case (_, []) where row.allSatisfy {$0 != "#"}: 1
+      case (_, []): 0
+      case (_, _) where row.first == ".": waysOfPuttingSprings((row.dropFirst(), conditions))
+      case (_, _) where row.first == "#": {
         let springsToPut = conditions[0]
-        let availableSpace = row.count - row.trimmingPrefix(while: { $0 == "?" || $0 == "#" }).count
+        let availableSpace = row.count - row.trimmingPrefix(while: {$0 == "?" || $0 == "#"}).count
 
-        return
-        if availableSpace < springsToPut
-          || (springsToPut < row.count
-            && row[row.index(row.startIndex, offsetBy: springsToPut)] == "#")
-        {
+        return if availableSpace < springsToPut || 
+          (springsToPut < row.count && row[row.index(row.startIndex, offsetBy: springsToPut)] == "#") {
           0
-        } else {
+        }
+        else {
           waysOfPuttingSprings((row.dropFirst(springsToPut + 1), Array(conditions.dropFirst())))
         }
       }()
-    case (_, _) where row.first == "?":
-      {
+      case (_, _) where row.first == "?": {
         waysOfPuttingSprings(("#" + row.dropFirst(), conditions))
           + waysOfPuttingSprings((row.dropFirst(), conditions))
       }()
-    case (_, _): 0
+      case (_, _): 0
     }
 
     calculatedWays[RowAndConditions(row: row, conditions: conditions)] = calculated
     return calculated
   }
 }
+
 
 print(input.map { waysOfPuttingSprings($0) }.reduce(0, +))
 
